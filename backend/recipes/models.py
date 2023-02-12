75619d376a637da3eb7e8d4d2137ab2e2b4344ac
Matheus Cafalchio <maolivei@tcd.ie>
2023-02-12 21:34:42 +0000

    re-statarted backend, added models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Recipe(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
+    title = models.CharField(max_length=50 )
+    description = models.TextField()
+    cooking_time = models.CharField(max_length=50)
+    servings = models.SmallIntegerField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    servings = models.CharField(max_length=50)
+    image = models.URLField(max_length=200, blank=True, null=True)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-    
-    
