20b61276e23930b887b59694d74877628d3d08d9
matheus <matheusoliveira@stratum.ie>
2023-02-18 14:10:47 +0000

    api endpoints working

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Comment(models.Model):
+    recipe = models.ForeignKey('recipes.Recipe', on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.recipe}'
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
+
+"""
+    Custom user model to have email as the username
+    based on: https://www.youtube.com/watch?v=mndLkCEiflg
+"""
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    #regular user
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    #superuser
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, blank=False, null=False)
+    username = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        
+    def __str__(self):
+        return self.email
+    
