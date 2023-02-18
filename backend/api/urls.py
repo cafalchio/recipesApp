106bb774aeefe127eb58f6c36dd8e75662597460
matheus <matheusoliveira@stratum.ie>
2023-02-18 13:50:40 +0000

    added recipe serializer, views  and urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from recipes import views as recipes_views
+from comments import views as comments_views
+from rest_framework.urlpatterns import format_suffix_patterns
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('recipes.urls')),
-    # path('api/', include('comments.urls')),
-    # path('api/', include('users.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/recipes', recipes_views.recipe_list),
+    path('api/recipes/<int:pk>', recipes_views.recipe_detail),
+    path('api/comments',  comments_views.comment_list),
+    path('api/comments/<int:pk>', comments_views.comment_detail)
+    # # path('api/', include('comments.urls')),
+    # # path('api/', include('users.urls')),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
 class Comment(models.Model):
     comment = models.TextField()
-    stars = models.SmallIntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
+    stars = models.SmallIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     recipe = models.ForeignKey('recipes.Recipe', on_delete=models.CASCADE)
 
